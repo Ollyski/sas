391480baf7a19125e9bef95d1cef48cfda583d0c
Ollyski <146211302+Ollyski@users.noreply.github.com>
2023-12-13 10:05:18 -0500

    Add files via upload

---
+++ Phase05/private/db_credentials.php
@@ -0,0 +1,15 @@
+<?php
+
+  //local
+  //define("DB_SERVER", 'localhost');
+  //define("DB_USER", 'sally');
+  //define("DB_PASS", 'password');
+  //define("DB_NAME", 'salamanders');
+  
+  //webhost
+  //define("DB_SERVER", "");
+  //define("DB_USER", "");
+  //define("DB_PASS", "");
+  //define("DB_NAME", "");
+
+?> -->
