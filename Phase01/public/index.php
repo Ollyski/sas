1e0e02c6c920d4adafde66a50e2fd027fc6522fa
Ollyski <146211302+Ollyski@users.noreply.github.com>
2023-12-12 22:52:43 -0500

    Add files via upload

---
+++ Phase01/public/index.php
@@ -0,0 +1,22 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php 
+$pageTitle = 'Welcome to SAS';
+include_once(SHARED_PATH . '/salamander-header.php');
+?>
+<main>
+  <h1>Welcome to SAS</h1>
+  <p>The Southern Appalachian Mountains Region is often hailed as the Salamander Capital of the World. In fact, the Smithsonian Conservation Biology Institute proclaims that the Appalachian region is home to more salamander species than anywhere else in the world, making it a true hotspot for salamander biodiversity.</p> 
+  <li>
+    <ul><a href="phase01">Phase 01</a></ul>
+    <ul><a href="phase02">Phase 02</a></ul>
+    <ul><a href="phase03">Phase 03</a></ul>
+    <ul><a href="phase04">Phase 04</a></ul>
+    <ul><a href="phase05">Phase 05</a></ul>
+  </li>
+
+  <p><a href="https://www.savethesalamanders.com/appalachian-salamanders/">Source</a></p>
+</main>
+<footer>
+  <?php include_once(SHARED_PATH . '/salamander-footer.php'); ?>
+</footer>
